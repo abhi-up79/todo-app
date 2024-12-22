1a7b2a5279fa372128c6fb91c72e2992c87f60fa
Abhishek Sharma <sharma.abhishek208@gmail.com>
2024-12-22 11:55:36 +0530

    Added Implementation for Todo Rest CRUD

---
+++ backend-springboot/src/main/java/com/abhi/todo/controller/TodoController.java
@@ -0,0 +1,54 @@
+package com.abhi.todo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.abhi.todo.model.Todo;
+import com.abhi.todo.service.TodoService;
+
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+    @Autowired
+    private TodoService service;
+
+    @GetMapping
+    public List<Todo> getTodos() {
+        return service.getAllTodos();
+    }
+
+    @PostMapping
+    public Todo addTodo(@RequestBody Todo todo) {
+        return service.saveTodo(todo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable Long id, @RequestBody Todo updatedTodo) {
+        Optional<Todo> existingTodo = service.getTodoById(id);
+        if (existingTodo.isPresent()) {
+            Todo todo = existingTodo.get();
+            todo.setTitle(updatedTodo.getTitle());
+            todo.setCompleted(updatedTodo.isCompleted());
+            return ResponseEntity.ok(service.saveTodo(todo));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable Long id) {
+        service.deleteTodo(id);
+    }
+}
